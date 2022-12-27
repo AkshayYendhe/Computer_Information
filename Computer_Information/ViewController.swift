7c1ee2185bc7c7eaf407d79ec774d90e29032793
forloop <yendheakshayd@gmail.com>
2022-12-27 12:54:10 +0530

    Initial Commit

---
+++ Computer_Information/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Computer_Information
+//
+//  Created by Akshay Yendhe on 27/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
